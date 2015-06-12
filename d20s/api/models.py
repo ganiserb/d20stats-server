8353f729ab4d763f583fa7833f456b03f5aaa91f
ganiserb <g.gabriel.b@gmail.com>
2015-06-12 19:35:39 -0300

    createdd first model

--- d20s/api/models.py
+++ d20s/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Roll(models.Model):
+    """
+    This represents a single roll entry
+    """
+    roll = models.PositiveSmallIntegerField(
+        verbose_name='roll number',
+        help_text='The number rolled on the d20 dice'
+    )
+    player = models.ForeignKey(
+        User,
+        help_text='The player that made the roll'
+    )
+    moment_rolled = models.DateTimeField(
+        auto_now_add=True,
+        help_text='The moment the roll was registered'
+    )
