1d4a852f7a7bcce2ad16386299d385eb9bc004c1
ganiserb <g.gabriel.b@gmail.com>
2015-06-12 23:01:01 -0300

    improving the api

--- d20s/registrator/api.py
+++ d20s/registrator/api.py
@@ -3,14 +3,19 @@ __author__ = 'gabriel'
 from django.contrib.auth.models import User
 from registrator.models import Roll
 from tastypie.resources import ModelResource
-
-
-class RollResource(ModelResource):
-    class Meta:
-        queryset = Roll.objects.all()
+from tastypie import fields
 
 
 class UserResource(ModelResource):
     class Meta:
         queryset = User.objects.filter(is_superuser=False)
         fields = ['username', 'first_name', 'last_name']
+        allowed_methods = ['get']
+
+
+class RollResource(ModelResource):
+    class Meta:
+        queryset = Roll.objects.all()
+        player = fields.ToOneField(UserResource, 'player')
+        # fields = ['roll', 'player']
+        allowed_methods = ['get', 'post']
