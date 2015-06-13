60e670997f04f17f9ff3f8fe6e1ff289e1b01853
ganiserb <g.gabriel.b@gmail.com>
2015-06-12 21:20:40 -0300

    updated crossdomain thing for flash post

--- d20s/registrator/views.py
+++ d20s/registrator/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, FileResponse
 
 
 # Create your views here.
 def test(request):
     return HttpResponse('test ok!')
 
+
 def crossdomain(request):
-    return HttpResponse("""<?xml version="1.0" encoding="utf-8"?>
+    response = FileResponse("""<?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE cross-domain-policy
   SYSTEM 'http://www.adobe.com/xml/dtds/cross-domain-policy.dtd'>
 <cross-domain-policy>
@@ -15,3 +16,5 @@ def crossdomain(request):
 	<allow-http-request-headers-from domain="d20s.herokuapp.com" headers="Authorization"/>
 </cross-domain-policy>
 """)
+    response['Content-Type'] = "text/x-cross-domain-policy; charset=utf-8"
+    return response
